cc7b26273f63707aa62dcc623bc91104320fcab4
serendipity0321 <1727439300@QQ.com>
2023-02-13 02:32:15 +0800

    项目初始化

---
+++ src/views/LinkView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="link">
+    <h1>This is an link page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .link {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
